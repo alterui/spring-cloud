f7fbf1e9103997fce0ba68c9b9249bba2acfd6f2
alterui <656812771@qq.com>
2020-04-06 12:14:55 +0800

    将paymentApplication服务提供者注册到eureka注册中心上面

--- cloud-provider-payment8001/src/main/java/com/lr/springcloud/Payment8001Application.java
+++ cloud-provider-payment8001/src/main/java/com/lr/springcloud/Payment8001Application.java
@@ -2,12 +2,14 @@ package com.lr.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author liurui
  * @date 2020/3/30 6:22 下午
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class Payment8001Application {
     public static void main(String[] args) {
         SpringApplication.run(Payment8001Application.class,args);
