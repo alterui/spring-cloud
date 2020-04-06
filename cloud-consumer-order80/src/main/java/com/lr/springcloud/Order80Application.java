97f28408584e7602e0e7374876b6f23f5e863ca0
alterui <656812771@qq.com>
2020-04-06 14:21:11 +0800

    将orderApplication服务消费者注册到eureka注册中心上面

--- cloud-consumer-order80/src/main/java/com/lr/springcloud/Order80Application.java
+++ cloud-consumer-order80/src/main/java/com/lr/springcloud/Order80Application.java
@@ -2,12 +2,14 @@ package com.lr.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author liurui
  * @date 2020/4/4 16:50
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class Order80Application {
     public static void main(String[] args) {
         SpringApplication.run(Order80Application.class, args);
