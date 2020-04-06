95597a94036567118d1f34aacf6475434dc7c042
alterui <656812771@qq.com>
2020-04-06 17:06:32 +0800

    eureka集群搭建

---
+++ cloud-eureka-server7002/src/main/java/com/lr/springcloud/Eureka7002Application.java
@@ -0,0 +1,19 @@
+package com.lr.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author liurui
+ * @date 2020/4/6 16:45
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Eureka7002Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Eureka7002Application.class, args);
+    }
+
+}
